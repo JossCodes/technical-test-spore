d332965f77218654b4e7701be52331a29a0f9f19
Josue <79381280+JossCodes@users.noreply.github.com>
2023-08-06 18:42:33 -0600

    se elimina carpeta api/dist

--- api/dist/database/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_1 = require("sequelize");
-const config = require("./config");
-exports.sequelize = new sequelize_1.Sequelize(config.development.database, config.development.username, config.development.password, {
-    host: config.development.host,
-    dialect: config.development.dialect,
-});
